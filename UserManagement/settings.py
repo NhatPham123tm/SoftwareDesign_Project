68936bf2ac204bb63f69f1342684458456f116db
Tien Phu Tran <phu27312345@gmail.com>
2025-02-14 14:14:31 -0600

    add templates direction in setting

--- UserManagement/settings.py
+++ UserManagement/settings.py
@@ -74,7 +74,7 @@ ROOT_URLCONF = 'UserManagement.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
