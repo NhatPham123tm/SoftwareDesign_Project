d30b7ee1b759ffd2a344db649c7cb0a9f8d0597a
Tien Phu Tran <phu27312345@gmail.com>
2025-02-17 12:36:44 -0600

    Temporary change homepage to login page

--- UserManagement/urls.py
+++ UserManagement/urls.py
@@ -3,12 +3,12 @@ from django.urls import path, include
 from users.views import home  # Import the home view
 from django.contrib.auth import views as auth_view
 from users.views import microsoft_callback 
-from users.views import dashboard
+from users.views import dashboard, user_login
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),  # Keeps user-related URLs
-    path('', home, name='home'),  # Define the root URL
+    path('', user_login, name='home'),  # Define the root URL
     path('auth/complete/azure/', microsoft_callback, name='microsoft-callback'),
     path('dashboard/', dashboard, name='dashboard'),
     path('api/', include('api.urls')),
