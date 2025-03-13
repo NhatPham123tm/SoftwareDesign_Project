5b77f3e20f0adc8b1abdff2d5aff1cfc07c33e66
Nhat Pham <strikefreedom779@fmail.com>
2025-03-13 17:25:20 -0500

    Add testing for pdf generate

--- UserManagement/urls.py
+++ UserManagement/urls.py
@@ -4,6 +4,7 @@ from authentication.views import home  # Import the home view
 from authentication.views import microsoft_callback, dashboard, user_login, register_page ,microsoft_login,microsoft_logout, login_page, user_register, basicuser, adminpage, get_userLoad, reset_password,suspend, get_auth_data
 from authentication import views
 from django.contrib.auth import views as auth_views
+from formProcessor.views import generate_reimburse_pdf
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,4 +25,5 @@ urlpatterns = [
     path('api/get_userLoad/', get_userLoad, name='get_userLoad'),
     path('suspend/', suspend, name='suspend'),
     path("api/microsoft-login/", get_auth_data, name="microsoft-login-json"),
+    path('generate_reimburse_pdf/', generate_reimburse_pdf, name='generate_pdf'),
 ]
