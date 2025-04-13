03153adcaec0920a030740caee0a657d59b6b223
TienPhuTran <phu27312345@gmail.com>
2025-04-13 18:08:58 -0500

    add user ura api

--- api/views.py
+++ api/views.py
@@ -2,8 +2,8 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import action
-from .models import user_accs, roles, permission, PayrollAssignment, ReimbursementRequest, ChangeOfAddress, DiplomaRequest
-from .serializers import UserSerializer, RoleSerializer, PermissionSerializer, PayrollAssignmentSerializer, ReimbursementRequestSerializer, ChangeOfAddressSerializer, DiplomaRequestSerializer
+from .models import user_accs, roles, permission, PayrollAssignment, ReimbursementRequest, ChangeOfAddress, DiplomaRequest, user_ura_accs
+from .serializers import UserSerializer, RoleSerializer, PermissionSerializer, PayrollAssignmentSerializer, ReimbursementRequestSerializer, ChangeOfAddressSerializer, DiplomaRequestSerializer, UserURASerializer
 import os
 import base64
 import requests
@@ -79,6 +79,50 @@ class UserViewSet(viewsets.ModelViewSet):
             user.delete()
             return Response(status=status.HTTP_204_NO_CONTENT)
         except user_accs.DoesNotExist:
+            return Response({'detail': 'User not found.'}, status=status.HTTP_404_NOT_FOUND)
+        
+class UserURAViewSet(viewsets.ModelViewSet):
+    queryset = user_ura_accs.objects.all()
+    serializer_class = UserURASerializer
+
+    # Read (Retrieve) by ID (GET)
+    def retrieve(self, request, *args, **kwargs):
+        try:
+            user = user_ura_accs.objects.get(id=kwargs['pk'])
+            serializer = UserURASerializer(user)
+            return Response(serializer.data)
+        except user_ura_accs.DoesNotExist:
+            return Response({'detail': 'User not found.'}, status=status.HTTP_404_NOT_FOUND)
+
+    # Update by ID (PUT)
+    def update(self, request, *args, **kwargs):
+        try:
+            user = user_ura_accs.objects.get(id=kwargs['pk'])
+            serializer = UserURASerializer(user, data=request.data, partial=False)  # partial=False ensures full update
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except user_ura_accs.DoesNotExist:
+            return Response({'detail': 'User not found.'}, status=status.HTTP_404_NOT_FOUND)
+    # Partially Update by ID (PATCH)
+    def partial_update(self, request, *args, **kwargs):
+        try:
+            user = user_ura_accs.objects.get(id=kwargs['pk'])
+            serializer = UserURASerializer(user, data=request.data, partial=True) 
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except user_ura_accs.DoesNotExist:
+            return Response({'detail': 'User not found.'}, status=status.HTTP_404_NOT_FOUND)
+    # Delete a User by ID (DELETE)
+    def destroy(self, request, *args, **kwargs):
+        try:
+            user = user_ura_accs.objects.get(id=kwargs['pk'])
+            user.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except user_ura_accs.DoesNotExist:
             return Response({'detail': 'User not found.'}, status=status.HTTP_404_NOT_FOUND)
         
 class PermissionViewSet(viewsets.ModelViewSet):
