3c35643189e92edc796f0c003d90b4783b2b947c
TienPhuTran <phu27312345@gmail.com>
2025-04-17 21:41:42 -0500

    work_assign api

--- api/views.py
+++ api/views.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import action
-from .models import user_accs, roles, permission, PayrollAssignment, ReimbursementRequest, ChangeOfAddress, DiplomaRequest, user_ura_accs
+from .models import user_accs, roles, permission, PayrollAssignment, ReimbursementRequest, ChangeOfAddress, DiplomaRequest, user_ura_accs, work_assign
 from .serializers import UserSerializer, RoleSerializer, PermissionSerializer, PayrollAssignmentSerializer, ReimbursementRequestSerializer, ChangeOfAddressSerializer, DiplomaRequestSerializer, UserURASerializer, RequestSerializer
 import os
 import base64
@@ -30,10 +30,53 @@ from django.views.decorators.csrf import ensure_csrf_cookie
 from django.http import JsonResponse
 from authentication.views import is_admin
 from rest_framework.permissions import BasePermission
+
 class RoleViewSet(viewsets.ModelViewSet):
     queryset = roles.objects.all()
     serializer_class = RoleSerializer
 
+class WorkAssignViewSet(viewsets.ModelViewSet):
+    queryset = work_assign.objects.all()
+    serializer_class = RequestSerializer
+
+    # Read (Retrieve) by ID (GET)
+    def retrieve(self, request, *args, **kwargs):
+        try:
+            user = work_assign.objects.get(id=kwargs['pk'])
+            serializer = RequestSerializer(user)
+            return Response(serializer.data)
+        except work_assign.DoesNotExist:
+            return Response({'detail': 'User not found.'}, status=status.HTTP_404_NOT_FOUND)
+    # Update by ID (PUT)
+    def update(self, request, *args, **kwargs):
+        try:
+            user = work_assign.objects.get(id=kwargs['pk'])
+            serializer = RequestSerializer(user, data=request.data, partial=False)  # partial=False ensures full update
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except work_assign.DoesNotExist:
+            return Response({'detail': 'User not found.'}, status=status.HTTP_404_NOT_FOUND)
+    # Partially Update by ID (PATCH)
+    def partial_update(self, request, *args, **kwargs):
+        try:
+            user = work_assign.objects.get(id=kwargs['pk'])
+            serializer = RequestSerializer(user, data=request.data, partial=True) 
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except work_assign.DoesNotExist:
+            return Response({'detail': 'User not found.'}, status=status.HTTP_404_NOT_FOUND)
+    # Delete a User by ID (DELETE)
+    def destroy(self, request, *args, **kwargs):
+        try:
+            user = work_assign.objects.get(id=kwargs['pk'])
+            user.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except work_assign.DoesNotExist:
+            return Response({'detail': 'User not found.'}, status=status.HTTP_404_NOT_FOUND)
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = user_accs.objects.all()
