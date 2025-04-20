3e925d06f23960d9acbd6df9f2f2ee174a06bd0d
TienPhuTran <phu27312345@gmail.com>
2025-04-20 14:29:49 -0500

    add delagate fields

--- api/models.py
+++ api/models.py
@@ -148,6 +148,7 @@ class work_assign(models.Model):
     ], default='Pending')
 
     system_generated = models.BooleanField(default=False) # For user submit form which is system generated
+    delegated = models.ForeignKey('work_assign', on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return f"WorkAssign #{self.id}"
